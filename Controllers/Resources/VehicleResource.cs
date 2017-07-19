69a0dd86dad9815d77e39fdfd037e667286ce874
onurcelikaslan <onur.celikaslan@egegen.com>
2017-07-19 10:34:49 +0300

    Creating API Resource

---
+++ Controllers/Resources/VehicleResource.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using vega.Models;
+
+namespace vega.Controllers.Resources
+{
+    public class ContactResource
+    {
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        [StringLength(255)]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Phone { get; set; }
+
+    }
+    public class VehicleResource
+    {
+        public int Id { get; set; }
+        public int ModelId { get; set; }
+        public bool IsRegistered { get; set; }
+        public ContactResource Contact { get; set; }
+        public ICollection<int> Features { get; set; }
+
+        public VehicleResource()
+        {
+            Features = new Collection<int>();
+        }
+    }
+}
